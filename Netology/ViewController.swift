4640e5d033c48a84f2c4ab56af852cb72f734497
Igor Chaporgin <fellclub@yandex.ru>
2023-11-27 15:05:42 +0300

    First commit

---
+++ Netology/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Netology
+//
+//  Created by Igor Chaporgin on 26.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        super.view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
